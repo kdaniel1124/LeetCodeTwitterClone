68bca7d8eaacb94321644fa38dc220befdedfca6
kdaniel1124 <kennellydanny@gmail.com>
2023-10-05 13:40:25 -0400

    Pushing working leetcode twitter, except getnewsfeed method

---
+++ TwitterJunior/TwitterJunior/Program.cs
@@ -0,0 +1,27 @@
+﻿using TwitterJunior;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Twitter twit = new Twitter();
+        twit.PostTweet(1, 2);
+        twit.PostTweet(1, 4);
+        twit.PostTweet(2, 6);
+        twit.PostTweet(2, 8);
+        twit.PostTweet(3, 10);
+        twit.PostTweet(3, 12);
+        twit.PostTweet(4, 14);
+        twit.PostTweet(4, 16);
+
+        IList<int> feed = twit.GetNewsFeed(1);
+
+        foreach(int tweet in feed)
+        {
+            Console.WriteLine(tweet);
+        }
+        Console.WriteLine(twit.Tweets[0]);
+
+    }
+}
---
+++ TwitterJunior/TwitterJunior/Twitter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TwitterJunior
+{
+    public class Twitter
+    {
+        public Twitter()
+        {
+            this.UserFollowing = new Dictionary<int, List<int>>();
+            this.TweetsOwners = new Dictionary<int, int>();
+            this.Tweets = new List<int>();
+        }
+
+
+        public Dictionary<int, List<int>> UserFollowing { get; set; }
+
+        public Dictionary<int, int> TweetsOwners { get; set; }
+
+        public List<int> Tweets { get; set; }
+
+
+        public void PostTweet(int userId, int tweetId)
+        {
+            TweetsOwners[tweetId] = userId;
+            Tweets.Add(tweetId);
+
+        }
+
+        public IList<int> GetNewsFeed(int userId)
+        {
+            List<int> personalFeed = new List<int>();
+
+            if (!UserFollowing.ContainsKey(userId))
+            {
+                UserFollowing[userId] = new List<int>();
+            }
+
+            for (int i = Tweets.Count - 1; i >= 0; i--)
+            {
+                if (UserFollowing[userId].Contains(TweetsOwners[Tweets[i]]) || (TweetsOwners[Tweets[i]] == userId))
+                {
+                    personalFeed.Add(Tweets[i]);
+                }
+            }
+
+            return personalFeed;
+
+        }
+
+        public void Follow(int followerId, int followeeId)
+        {
+            // If follower isn't already following followee then follower's id gets added to followee's list
+            if (!UserFollowing[followerId].Contains(followeeId))
+            {
+                UserFollowing[followerId].Add(followeeId);
+            }
+
+        }
+
+        public void Unfollow(int followerId, int followeeId)
+        {
+            // If follower is following followee then follower's id gets removed from followee's list
+            if (UserFollowing[followerId].Contains(followeeId))
+            {
+                UserFollowing[followerId].Remove(followeeId);
+            }
+
+        }
+    }
+}
